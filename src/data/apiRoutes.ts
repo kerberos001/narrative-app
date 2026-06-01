4db5d761a5ae2291ced5a3644cc4dce4832a0230
Jorge Montenegro <jmontenegro@technisys.com>
2026-06-01 12:39:48 -0500

    Fix: rutas

--- src/data/apiRoutes.ts
+++ src/data/apiRoutes.ts
@@ -1,6 +1,7 @@
 // 1. Leemos la IP del archivo .env usando la sintaxis nativa de Vite
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/';
 
+console.log("ESTA ES LA RUTA QUE ESTA MAL: "+BASE_URL)
 // 2. Definimos y exportamos las constantes de las rutas de tu API
 export const API_ROUTES = {
   // Ruta dinámica para el player (recibe el username)
